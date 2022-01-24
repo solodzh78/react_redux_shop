5f6660121405466a8044b8b52fea0e1db7f24e6e
Sergei Solodzhuk <solodzh@gmail.com>
2022-01-24 06:50:58 +0200

    опубликовал на github-pages

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ function App({ setBooks, books, isReady }) {
   useEffect(() => {
     try {
       (async() => {
-        const { data } = await axios.get('/books.json');
+        const { data } = await axios.get('books.json');
         setBooks(data);
       })();
     } catch (error) {
